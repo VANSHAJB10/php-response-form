75724107cc00e8bf5065ae39ffd879dbdc3025a8
VANSHAJB10 <vanshajbajaj1002@gmail.com>
2024-05-29 22:49:00 +0530

    added php functions to handle POST events.

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+$insert = false;
+if(isset($_POST['name']))
+{
+
     $server = "localhost";
     $username = "root";
     $password = "";
@@ -8,5 +12,24 @@
     if(!$con){
         die("⚠ Connection to Database ❌Failed!❌ due to " .mysqli_connect_eror() );
     }
-    echo "Successful connection to Db ✅"; 
+    // echo "Successful connection to Db ✅"; 
+
+    //POST Variables
+    $name = $_POST['name'];
+    $id = $_POST['id'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $message = $ POST['message'];
+
+     $sql = "INSERT INTO `Transaction Form`.`Transaction FORM` (`name`, `id`, `phone`, `email`, `message`) VALUES ('$name', '$id', '$phone', '$email', '$message', current_timestamp());";
+
+     if($con->query($sql) == true){
+        $insert = true;
+      }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+      }
+    $con->close();
+}
+
 ?>
