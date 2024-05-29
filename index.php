bff4d36702d55850350c8db8f4fe2061a40ed5eb
VANSHAJB10 <vanshajbajaj1002@gmail.com>
2024-05-29 22:52:54 +0530

    Form Submission event handled ✅.     Message Displayed
    	modified:   style.css

--- index.php
+++ index.php
@@ -33,3 +33,43 @@ if(isset($_POST['name']))
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Information</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="formContainer">
+        <h2>Hi</h2>
+        <p> Enter your details for Confirmation!</p>
+     
+    <?php
+        if($insert == true){
+        echo "<p class='.submitMesssage'>Your Response is Recorded ✅</p>";
+        }
+    ?>
+
+    <form actions = "index.php" method="post">
+        <input type ="text" name ="name" palceholder ="Name">
+
+        <input type ="number" name="id" placeholder="Id">
+
+        <input type="number" name ="phone" placeholder="Phone Number">
+
+        <input type="text" name="email" placeholder="Email Id">
+
+        <textarea name="message" id="message" cols="15" rows="10" placeholder="Message"></textarea>
+
+        <button class="btn" >Submit</button>
+        <button class="btn" >Reset↺</button>
+
+    </form>
+
+    </div>
+     
+</body>
+</html>
